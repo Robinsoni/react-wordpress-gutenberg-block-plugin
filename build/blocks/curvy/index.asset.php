075d92ef61dc27885ca29f56bef60ce68b666fea
Robin Soni <sonirobin369@gmail.com>
2024-04-04 12:28:43 +0530

    added block variation

--- build/blocks/curvy/index.asset.php
+++ build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '18b8c91bc965a641dff1');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b1cf806000092fa41557');
