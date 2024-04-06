48e829b817d6c050f6e815f9ddb0d2b3b337487f
Robin Soni <sonirobin369@gmail.com>
2024-04-06 19:46:43 +0530

    added gap parsing to clickyGroup

--- src/blocks/clickyGroup/edit.js
+++ src/blocks/clickyGroup/edit.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import {useBlockProps, useInnerBlocksProps} from '@wordpress/block-editor';
-export default function Edit(props){
-	const {className, ...blockProps} = useBlockProps();
-	const innerBlocksProps = useInnerBlocksProps(blockProps,{
-		template: [["blockylicious/clicky-button"]],
-		allowedBlocks:["blockylicious/clicky-button"]
+import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
+import { parseValue } from '../utils/parseValue';
+export default function Edit(props) {
+	const blockGap = parseValue(props.attributes?.style?.spacing?.blockGap || "");
+	const { className, ...blockProps } = useBlockProps({
+		style: {
+			gap: blockGap
+		}
 	});
-  return (
- 
-		<div {...innerBlocksProps} className={`${className} alignfull` }></div>
 	 
-  )
+	const innerBlocksProps = useInnerBlocksProps(blockProps, {
+		template: [["blockylicious/clicky-button"]],
+		allowedBlocks: ["blockylicious/clicky-button"]
+	});
+	return (
+
+		<div {...innerBlocksProps} className={`${className} alignfull`}></div>
+
+	)
 }
