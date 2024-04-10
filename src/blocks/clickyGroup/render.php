16c9b583b8e5763704c2c84b469feadcee63d68d
Robin Soni <sonirobin369@gmail.com>
2024-04-10 12:38:55 +0530

    refactor block registration code

--- src/blocks/clickyGroup/render.php
+++ src/blocks/clickyGroup/render.php
@@ -1,7 +1,7 @@
 
 <?
-
-$block_gap = convert_custom_properties($attributes['style']['spacing']['blockGap'] ?? 0);
+use BlockyliciousPlugins\Blockylicious;
+$block_gap = Blockylicious::convert_custom_properties($attributes['style']['spacing']['blockGap'] ?? 0);
 $block_attributes = get_block_wrapper_attributes([
     'style' => 'gap: ' . $block_gap . '; justify-content: ' . $attributes['justifyContent'],
     "class" => 'alignfull'
